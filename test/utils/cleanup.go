329acd14b578183c1f6582488f48e6f88ca6d6c4
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 15:22:34 +0200

    Update copyright text
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- test/utils/cleanup.go
+++ test/utils/cleanup.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // This file was copied from the kubernetes/kubernetes project
 // https://github.com/kubernetes/kubernetes/blob/v1.19.0/test/e2e/framework/cleanup.go
-// Modifications SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and Gardener contributors
+// Modifications SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
 
 package utils
 
